a495cca9750420eac006220cd755a9ab8368e080
Subham Kar Chowdhury <karchowdhurysubham@gmail.com>
2023-02-21 20:51:19 +0530

    landing page stage 1

---
+++ locality_frontend/src/components/Navbar.js
@@ -0,0 +1,69 @@
+import logo from '../assests/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand">
+          <img src={logo} alt="locality"/>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-center text-center"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav ">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-primary">
+            Left
+          </button>
+          <button type="button" class="btn btn-primary">
+            Middle
+          </button>
+          <button type="button" class="btn btn-primary">
+            Right
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
